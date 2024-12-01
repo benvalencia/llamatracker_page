42d333e78329f6632eb40642314f44f7f321b1db
Ben <benyamin.ak@hotmail.com>
2024-12-01 21:18:21 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import tailwind from "@astrojs/tailwind";
 const LIVE_URL = 'https://www.llamatracker.net';
+
 // https://astro.build/config
 export default defineConfig({
-   site: LIVE_URL,
+  site: LIVE_URL,
+  integrations: [tailwind()]
 });
