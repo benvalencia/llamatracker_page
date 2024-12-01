11b795dcdd2df2a7a6262bf016af2a383d63f288
Ben <benyamin.ak@hotmail.com>
2024-12-01 18:17:36 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-const LIVE_URL = 'https://llamatracker.net';
+const LIVE_URL = 'https://www.llamatracker.net';
 // https://astro.build/config
 export default defineConfig({
    site: LIVE_URL,
